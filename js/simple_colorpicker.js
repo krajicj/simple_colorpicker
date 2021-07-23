21d0c6fa8fd28f4fce18bfa1d38d09a58fd43840
krajicj <krajicj@gmail.com>
2021-07-23 22:56:59 +0200

    Dynamic position of the picker

--- js/simple_colorpicker.js
+++ js/simple_colorpicker.js
@@ -13,12 +13,14 @@ function SimpleColorpicker() {
   //Make context of this function visible in all code
   let _this = this;
   let _options = {};
+  let _arrowHeight = 10;
 
   //Set options or the default ones
   this.setOptions = (options) => {
     _options = {
       iconSize: options.iconSize,
       gridCols: options.gridCols,
+      position: options.position || "left",
       colors: options.colors || [
         "#3F51B5",
         "#2196F3",
@@ -44,6 +46,7 @@ function SimpleColorpicker() {
     };
   };
 
+  //Function that init the colorpicker
   this.colorpicker = (el = null) => {
     //Elements to bind colorpickers
     let elements = [];
@@ -117,18 +120,19 @@ function SimpleColorpicker() {
 
     //Bind showing
     pickerIcon.addEventListener("click", () => {
-      togglePicker(picker);
+      togglePicker(picker, pickerIcon);
     });
   }
 
   //Show and hide picker
-  function togglePicker(picker) {
+  function togglePicker(picker, pickerIcon) {
     if (picker.classList.contains("active")) {
       picker.classList.remove("active");
       picker.style.display = "none";
     } else {
       picker.classList.add("active");
       picker.style.display = "block";
+      placePicker(picker, pickerIcon, _options.position);
     }
   }
 
@@ -140,6 +144,99 @@ function SimpleColorpicker() {
     togglePicker(picker);
     //Set icon new color
     pickerIcon.style.backgroundColor = colorItem.dataset.color;
+  }
+
+  //Place picker to the specified position if there is a place
+  function placePicker(picker, pickerIcon, position) {
+    const pickerBound = picker.getBoundingClientRect();
+    const iconBound = pickerIcon.getBoundingClientRect();
+
+    const allPostions = ["top", "right", "bottom", "left"];
+
+    let coords = calculateCoords(pickerBound, iconBound, position);
+    //If does not fit check other position
+    if (!coords.doesFit) {
+      for(let i = 0; i < allPostions.length; i++){
+        coords = calculateCoords(pickerBound, iconBound, allPostions[i]);
+        console.log(position);
+        console.log(coords);
+
+        if (coords.doesFit) break;
+      }
+    }
+    console.log(coords);
+
+    picker.style.top = `${coords.top}px`;
+    picker.style.left = `${coords.left}px`;
+  }
+
+  function calculateCoords(pickerBound, iconBound, position) {
+    let top, left;
+    switch (position) {
+      case "top":
+        left = iconBound.left + iconBound.width / 2 - pickerBound.width / 2;
+        top = iconBound.top - pickerBound.height - _arrowHeight;
+        break;
+      case "right":
+        left = iconBound.left + iconBound.width + _arrowHeight;
+        top = iconBound.top + iconBound.height / 2 - pickerBound.height / 2;
+        break;
+      case "bottom":
+        left = iconBound.left + iconBound.width / 2 - pickerBound.width / 2;
+        top = iconBound.top + iconBound.height + _arrowHeight;
+        break;
+      case "left":
+        left = iconBound.left - pickerBound.width - _arrowHeight;
+        top = iconBound.top + iconBound.height / 2 - pickerBound.height / 2;
+        break;
+    }
+
+    //Check enought place
+    const doesFit = checkPositionPlace(pickerBound.width, pickerBound.height, {
+      left,
+      top,
+    }, position);
+
+    return { left, top, doesFit };
+  }
+
+  function checkPositionPlace(width, height, coords, position) {
+    let fit = true;
+    switch (position) {
+      case "top":
+        if (
+          coords.top < 0 ||
+          coords.left < 0 ||
+          coords.left + width > window.innerWidth
+        )
+          fit = false;
+        break;
+      case "right":
+        if (
+          coords.left + width > window.innerWidth ||
+          coords.top < 0 ||
+          coords.top + height + _arrowHeight > window.innerHeight
+        )
+          fit = false;
+        break;
+      case "bottom":
+        if (
+          coords.top + height + _arrowHeight > window.innerHeight ||
+          coords.left < 0 ||
+          coords.left + width > window.innerWidth
+        )
+          fit = false;
+        break;
+      case "left":
+        if (
+          coords.left < 0 ||
+          coords.top < 0 ||
+          coords.top + height + _arrowHeight > window.innerHeight
+        )
+          fit = false;
+        break;
+    }
+    return fit;
   }
 }
 
